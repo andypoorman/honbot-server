78f83a16275e3b1f7e75353655be9d4d3278eafd
cooper <scttcper@gmail.com>
2015-06-14 14:29:09 -0700

    add indexing to a few things and make some keys unique

--- src/setup.js
+++ src/setup.js
@@ -4,23 +4,16 @@ var config = require('../config');
 mongo.connect(config.db, function(err, db) {
     var players = db.collection('players');
     var matches = db.collection('matches');
-    players.ensureIndex({'nick': 1}, {unique: true}, function(){
-        players.ensureIndex({'account_id': 1}, {unique: true}, function(){
-            matches.ensureIndex({'players.player_id': 1}, function(){
-                matches.ensureIndex({'mode': 1}, function(){
-                    matches.ensureIndex({'id': 1}, function(){
-                        db.createCollection('apilogger', {max: 50000}, function(){
-                            db.close();
-                        });
+    players.ensureIndex({'nick': 1, 'account_id': 1}, {unique: true}, function(){
+        matches.ensureIndex({'players.player_id': 1, 'mode': 1}, function(){
+            matches.ensureIndex({'id': 1}, {unique: true}, function(){
+                db.createCollection('apilogger', {max: 50000}, function(){
+                    db.collection('apilogger').ensureIndex({'status': 1, 'date': 1}, function(){
+                        db.close();
                     });
                 });
             });
         });
     });
-
-    
-    
-    
-
 
 });
